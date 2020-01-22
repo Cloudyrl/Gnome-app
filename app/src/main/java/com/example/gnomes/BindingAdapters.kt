282671bfb8c2e03f71b64c56e0c627af6e42f8fe
Rommel Liendo <cloudyrl1@gmail.com>
2020-01-22 01:15:01 -0400

    BindingAdapter class added with the glide image fetch logic. Added animations for loading imgs too

---
+++ app/src/main/java/com/example/gnomes/BindingAdapters.kt
@@ -0,0 +1,21 @@
+package com.example.gnomes
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("thumbnailUrl")
+fun bindImage(imgView: ImageView, thumbnailUrl:String?){
+    thumbnailUrl?.let{
+        val imgUri = thumbnailUrl.toUri().buildUpon()
+            .scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
+    }
+}
