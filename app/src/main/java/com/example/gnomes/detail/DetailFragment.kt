38ec371ff5accf326b002362a54f1dca234dee29
Rommel Liendo <cloudyrl1@gmail.com>
2020-01-23 02:58:35 -0400

    GnomeListAdapter adapter binded to detail fragment and list_item

--- app/src/main/java/com/example/gnomes/detail/DetailFragment.kt
+++ app/src/main/java/com/example/gnomes/detail/DetailFragment.kt
@@ -20,10 +20,13 @@ class DetailFragment : Fragment() {
         val application = requireNotNull(activity).application
         val binding = DetailFragmentBinding.inflate(inflater)
         binding.setLifecycleOwner(this)
+
         val gnomeModel = DetailFragmentArgs.fromBundle(arguments!!).selectedGnome
         val viewModelFactory = DetailViewModelFactory(gnomeModel,application)
         binding.viewModel = ViewModelProviders.of(
             this,viewModelFactory).get(DetailViewModel::class.java)
+        binding.professionslist.adapter = GnomeListAdapter(gnomeModel.professions,application)
+        binding.friendslist.adapter = GnomeListAdapter(gnomeModel.friends,application)
         return binding.root
     }
 
